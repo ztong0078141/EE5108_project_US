841fb27b9f3209fa303c0b61a0775828c6449032
ztong0078141 <ztong0078141>
2016-10-14 11:49:29 +0800

    gpiolib plus ledblink trial

--- app/src/main/java/com/example/zhaotong/db410c_system/MainActivity.java
+++ app/src/main/java/com/example/zhaotong/db410c_system/MainActivity.java
@@ -3,11 +3,42 @@ package com.example.zhaotong.db410c_system;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.calit2.mooc.iot_db410c.db410c_gpiolib.GpioProcessor;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                GpioProcessor gpioProcessor = new GpioProcessor();
+
+                GpioProcessor.Gpio led = gpioProcessor.getPin34();
+                led.out();
+                led.low();
+
+                for(int i = 0; i<10;i++){
+                    led.high();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    led.low();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
     }
+
 }
