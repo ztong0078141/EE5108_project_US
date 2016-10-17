ed62ac096db5cd8a754d35391bcf2c00b82a075d
ztong0078141 <ztong0078141>
2016-10-17 16:33:14 +0800

    initial commit

--- app/src/main/java/com/example/zhaotong/db410c_system/MainActivity.java
+++ app/src/main/java/com/example/zhaotong/db410c_system/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.zhaotong.db410c_system;
 
-
-
-
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
 import net.calit2.mooc.iot_db410c.db410c_gpiolib.GpioProcessor;
 import net.calit2.mooc.iot_db410c.db410c_gpiolib.GpioProcessor.Gpio;
+
+import com.example.zhaotong.db410c_system.Bluetooth.BluetoothRemoteFragment;
 import com.example.zhaotong.db410c_system.UltrasonicSensor.UltrasonicSensorProcessor;
 
-
-public class MainActivity extends Activity {
-    private static final String TAG = "UltrasonicActivity";
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = "MainActivity";
 
     private GpioProcessor processor;
     private Gpio triggerPin;
@@ -28,7 +28,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        init();
+        //init();
+
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager()
+                    .beginTransaction();
+
+
+            /********************************************************************************
+             * Make sure you comment out one of the two following fragments
+             * depending on what kind of device you are deploying to
+             */
+            //BluetoothReceiverFragment fragment = BluetoothReceiverFragment.newInstance();
+            BluetoothRemoteFragment fragment = BluetoothRemoteFragment.newInstance();
+            /********************************************************************************/
+            transaction.replace(R.id.content_fragment, fragment);
+            transaction.commit();
+        }
     }
     private void init(){
         Log.i(TAG, "Initializing Variables");
